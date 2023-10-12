72cde8b13d21d4ab83afc83e0442a22608ab7528
Clark Weckmann <accounts@weckmann.me>
2023-10-12 10:11:43 -0500

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+// express
+const express = require('express');
+const app = express();
+// csv to json
+const csv = require('csvtojson');
+// fs
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
+const multer = require('multer');
+const upload = multer({
+    dest: 'public/tmp',
+});
+// path
+const path = require('path');
+// port
+const port = process.env.PORT || 3006;
+// handlebars
+const hbs = require('hbs')
+// view engine
+app.set('view engine', 'hbs');
+// views
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/upload', upload.single('csvFile'), (req, res) => {
+    const title = req.file.filename;
+    const file = req.file;
+    console.log(file);
+    const csvFilePath = file.path;
+    if (file.size > 1000000) {
+        res.send('File is too large. 1MB limit.');
+    }
+    if (file.mimetype !== 'text/csv') {
+        res.send('File is not a CSV.');
+    }
+
+    res.redirect(`/table/${title}`);
+
+});
+
+
+app.get('/table/:id', (req, res) => {
+    const csvFilePath = path.join(__dirname, 'public', 'tmp', `${req.params.id}`);
+    convertCSVtoJSON(csvFilePath).then((data) => {
+        res.render('table2', { data: encodeURIComponent(JSON.stringify(data)) });
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+);
+
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// }
+// );
+// list on port on host 0.0.0.0
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
+}
+);
+
+async function convertCSVtoJSON(csvFilePath) {
+    const jsonArray = await csv().fromFile(csvFilePath);
+    return jsonArray;
+}
---
+++ public/call.js
@@ -0,0 +1,135 @@
+// Sample JSON data
+const jsonData = JSON.parse(tableDataJson);
+const prettyClasses = {
+    th: 'bg-gray-300',
+    thFont: 'text-xl',
+    cellText: 'text-gray-800',
+    cellFont: 'text-lg',
+    telLink: 'text-blue-500'
+};
+const table = document.getElementById('data-table');
+
+// Function to render JSON data in the table
+function renderTable(data) {
+    // url is like /table/:id get the ID
+    const url = window.location.href;
+    const tableID = url.split('/')[4];
+    table.innerHTML = ''; // Clear the table
+
+    // Sort data alphabetically by 'Name' property
+    data.sort((a, b) => a.Name.localeCompare(b.Name));
+
+    // Create table headers
+    const headerRow = table.insertRow();
+    const checkboxHeader = document.createElement('th');
+    checkboxHeader.classList.add(prettyClasses.th);
+    checkboxHeader.textContent = 'Called';
+    headerRow.appendChild(checkboxHeader);
+
+    Object.keys(data[0]).forEach(key => {
+        const headerCell = document.createElement('th');
+        headerCell.classList.add(prettyClasses.th);
+        headerCell.classList.add(prettyClasses.thFont);
+        headerCell.textContent = key;
+        headerRow.appendChild(headerCell);
+    });
+
+    // Populate table rows with JSON data and add checkboxes
+    data.forEach(item => {
+        const row = table.insertRow();
+        const checkboxCell = row.insertCell();
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = localStorage.getItem(item.Name + '-' + tableID) === 'true'; // Set checkbox state from localStorage
+        checkbox.addEventListener('change', () => {
+            localStorage.setItem(item.Name + '-' + tableID, checkbox.checked); // Save checkbox state to localStorage
+            localStorage.setItem('list-' + tableID, JSON.stringify({
+                url: tableID,
+                name: JSON.parse(localStorage.getItem('list-' + tableID)).name,
+                date: JSON.parse(localStorage.getItem('list-' + tableID)).date,
+                lastUsed: new Date(),
+                notes: JSON.parse(localStorage.getItem('list-' + tableID)).notes,
+                contactCount: jsonData.length,
+                called: JSON.parse(localStorage.getItem('list-' + tableID)).called + (checkbox.checked ? 1 : -1),
+            }));
+
+            renderTable(data); // Re-render the table to reorder the rows based on 'Name' and checkbox state
+            allChecked();
+        });
+        checkboxCell.appendChild(checkbox);
+
+        Object.keys(item).forEach(key => {
+            const cell = row.insertCell();
+            cell.classList.add(prettyClasses.cellText);
+            cell.classList.add(prettyClasses.cellFont);
+            if (key === 'Phone') {
+                // For 'Phone' key, remove non-numeric characters and create tel: link
+                const phoneNumber = item[key].replace(/\D/g, ''); // Remove non-numeric characters
+                const telLink = document.createElement('a');
+                telLink.classList.add(prettyClasses.telLink);
+                telLink.href = `tel:${phoneNumber}`;
+                telLink.textContent = phoneNumber;
+                cell.appendChild(telLink);
+            } else {
+                cell.textContent = item[key];
+            }
+        });
+    });
+
+    // Move checked items to the bottom
+    const checkedRows = Array.from(table.rows).filter(row => {
+        const checkbox = row.cells[0].querySelector('input[type="checkbox"]');
+        return checkbox && checkbox.checked;
+    });
+    checkedRows.forEach(row => table.appendChild(row));
+}
+
+
+// Initial rendering of the table
+renderTable(jsonData);
+
+// url is like /table/:id get the ID
+const currentUrl = window.location.href;
+const urlToSave = currentUrl.split('/')[4];
+// save to local storage with json like {url: {name: '', date:'', notes:''}} unless it already exists
+const windowStorage = window.localStorage;
+const storedData = windowStorage.getItem('list-' + urlToSave);
+if (!storedData) {
+    windowStorage.setItem('list-' + urlToSave, JSON.stringify({
+        url: urlToSave,
+        name: 'Name This List',
+        date: new Date(),
+        lastUsed: new Date(),
+        notes: 'Add some notes!',
+        contactCount: jsonData.length,
+        called: 0
+    }));
+}
+const tableTitle = document.getElementById('table-title');
+tableTitle.innerHTML = JSON.parse(storedData).name;
+
+function addConfetti() {
+    // <script src="https://cdn.jsdelivr.net/npm/tsparticles-confetti@2.12.0/tsparticles.confetti.bundle.min.js"></script>
+    // confetti({
+    //     particleCount: 100,
+    //     spread: 70,
+    //     origin: { y: 0.6 },
+    // });
+    const confettiScript = document.createElement('script');
+    confettiScript.src = 'https://cdn.jsdelivr.net/npm/tsparticles-confetti@2.12.0/tsparticles.confetti.bundle.min.js';
+    document.body.appendChild(confettiScript);
+    confettiScript.onload = function () {
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 },
+        });
+    };
+}
+
+function allChecked() {
+    const checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+    if (checkboxes.every(checkbox => checkbox.checked)) {
+        addConfetti();
+    }
+}
